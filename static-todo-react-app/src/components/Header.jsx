5f148608c92ee657a89316c437a10c3621b24477
jennyo <jennifer.oleary.10@gmail.com>
2025-05-12 15:20:29 -0500

    complete to-do list

---
+++ static-todo-react-app/src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header>
+            <h2>My To-Do List</h2>
+        </header>
+    )
+}
+
+export default Header;
---
+++ static-todo-react-app/src/components/ToDoList.jsx
@@ -0,0 +1,18 @@
+
+const ToDoList = () => {
+    let arrTasks = ["Learn React", "Build a project", "Read documentation"];
+    return (
+        <div className="tasks">
+            <h3>To-Do List</h3>
+            <ul>
+                <li>Learn React</li>
+                <li>Build a project</li>
+                <li>Read documentation</li>
+            </ul>
+        </div>
+    )
+}
+
+
+
+export default ToDoList;
